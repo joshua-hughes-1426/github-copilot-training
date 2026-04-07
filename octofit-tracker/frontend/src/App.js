5f7a00ca128fd6bfc7edcd63366d87e321933c6c
joshua-hughes-1426 <joshua.hughes@accenture.com>
2026-04-07 19:49:22 +0000

    Step 5 Update 1

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,5 +1,12 @@
 
 
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
+import Activities from './components/Activities';
+import Leaderboard from './components/Leaderboard';
+import Teams from './components/Teams';
+import Users from './components/Users';
+import Workouts from './components/Workouts';
 import octofitLogo from './octofitapp-small.png';
 
 function App() {
